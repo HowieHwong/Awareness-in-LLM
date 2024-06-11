e3ba8e80cfe68f101377a59d86134e60a2bf9512
Yuan Li <58851983+realYuanLi@users.noreply.github.com>
2024-06-11 20:52:05 +0100

    Delete New/config.py

--- New/config.py
+++
@@ -1,4 +0,0 @@
-qwen_api_key = "sk-a1c07fe982064336b1d751cd287fae8c"
-openai_api = "428527aa7f804ebd866f9bb76bbe3ffe"
-deepinfra_api = "kI3jALDHZxJPSEdRJrN7v3RUFwaVJSUg"
-zhipu_api = "ee826135c72412acb4d0f5788f3290b3.Tzbtnvohm9MNHeck"
